67374bd7c7d680c385197135236db84935d2aaf0
Meha <ahem4progs@gmail.com>
2021-11-08 15:36:59 +0530

    Axios get call

--- src/components/hooks/customHooks/CustomHooksComponent.jsx
+++ src/components/hooks/customHooks/CustomHooksComponent.jsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
-function useAutoTimeHooks() {
+export function useAutoTimeHooks() {
+
+    useEffect(() => {
+        setInterval(() => {
+            Axios.get("https://priceapi.moneycontrol.com/pricefeed/bse/equitycash/SBI").then((response) => {
+                setSbi(response.data.data.pricecurrent);
+            })
+        }, 2000)
+    }, []);
+
     let date = new Date();
     function getCurrentTime() {
         return date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
     }
     let [time, setTime] = useState(getCurrentTime());
+    let [sbi, setSbi] = useState(0);
 
     setTimeout(() => {
         setTime(getCurrentTime());
     }, 1000);
     
-    return [time];
+    return [time, sbi];
 }
 
 
 export default function CustomHooksComponent() {
-    const [time] = useAutoTimeHooks();
+    const [time, sbi] = useAutoTimeHooks();
     return(
         <div>
             <h1>Current Time {time}</h1>
+            <h2>Price {sbi}</h2>
         </div>
     );
 }
